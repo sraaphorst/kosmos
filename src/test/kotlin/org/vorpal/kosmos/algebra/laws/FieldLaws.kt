1c9fe7137e537811795a9d9f9adb60290915877c
Sebastian Raaphorst <sraaphorst@gmail.com>
2025-08-24 21:45:33 -0400

    New arbs and some refactoring.

--- src/test/kotlin/org/vorpal/kosmos/algebra/laws/FieldLaws.kt
+++ src/test/kotlin/org/vorpal/kosmos/algebra/laws/FieldLaws.kt
@@ -6,7 +6,7 @@ import org.vorpal.kosmos.algebra.ops.Mul
 import org.vorpal.kosmos.algebra.structures.AbelianGroup
 import org.vorpal.kosmos.algebra.structures.Field
 import org.vorpal.kosmos.core.Eq
-import org.vorpal.kosmos.testing.excluding
+import org.vorpal.kosmos.testing.nonIdentity
 
 class FieldLaws<A>(
     F: Field<A>,       // has add: AbelianGroup<A, Add>, mul: AbelianGroup<A, Mul>
@@ -15,10 +15,8 @@ class FieldLaws<A>(
 ) {
     private val ringLaws = RingLaws(F, arb, EQ)
 
-    // Elements for the multiplicative group: F \ {0}
-    private val nonzero: Arb<A> = arb.excluding(EQ, F.add.identity)
-
     /** Multiplicative abelian group on F \ {0}. */
+    private val nonzero = nonIdentity(arb, F.add.identity, EQ)
     private val mulLaws = AbelianGroupLaws<A, AbelianGroup<A, Mul>>(F.mul, nonzero, EQ)
 
     suspend fun all() {
--- src/test/kotlin/org/vorpal/kosmos/testing/ArbExt.kt
+++
@@ -1,9 +0,0 @@
-package org.vorpal.kosmos.testing
-
-import io.kotest.property.Arb
-import io.kotest.property.arbitrary.filter
-import org.vorpal.kosmos.core.Eq
-
-/** Exclude any of the [banned] values using Eq. */
-fun <A> Arb<A>.excluding(eq: Eq<A>, vararg banned: A): Arb<A> =
-    this.filter { a -> banned.none { b -> eq.eqv(a, b) } }
