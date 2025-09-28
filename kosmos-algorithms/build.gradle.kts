ecb6c0ee71e98aa88cd310a8d0dc6bdda86aca21
Sebastian Raaphorst <sraaphorst@gmail.com>
2025-09-28 18:19:00 -0400

    Adding kosmos-algorithms build.gradle.kts.

---
+++ kosmos-algorithms/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins { kotlin("jvm") }
+group = "org.vorpal.kosmos.algorithms"
+version = "1.0-SNAPSHOT"
+
+// Some algorithms will require Lawful instances, so we want kosmos-lawkit as a dependency here.
+// TODO: re-add kosmos-lawkit once it is working.
+dependencies {
+    implementation(project(":kosmos-core"))
+//    implementation(project(":kosmos-lawkit"))
+}
