f415ff7e47ada413e7a2fcfc203f6d4c2e9bd989
Sebastian Raaphorst <sraaphorst@gmail.com>
2025-09-27 22:32:34 -0400

    K22: Initial documentation skeleton.

--- kosmos-core/src/main/kotlin/org/vorpal/kosmos/algebra/structures/Ring.kt
+++ kosmos-core/src/main/kotlin/org/vorpal/kosmos/algebra/structures/Ring.kt
@@ -2,9 +2,11 @@ package org.vorpal.kosmos.algebra.structures
 
 import org.vorpal.kosmos.core.ops.Add
 import org.vorpal.kosmos.core.ops.Mul
+import org.vorpal.kosmos.core.ops.OpTag
+import org.vorpal.kosmos.core.props.Distributivity
 
 /** Ring packs two ops with cross-properties (distributivity). */
-interface Ring<A, out M : Monoid<A, Mul>> {
+interface Ring<A> {
     val add: AbelianGroup<A, Add>
-    val mul: M
+    val mul: Monoid<A, Mul>
 }
