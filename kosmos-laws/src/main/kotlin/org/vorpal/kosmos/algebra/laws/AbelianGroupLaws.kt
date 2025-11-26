a4a71361bd1796d903f270861ab1f543ea0cf44e
Sebastian Raaphorst <sraaphorst@gmail.com>
2025-11-26 15:43:06 -0500

    K8b: Small changes and improvements.

--- kosmos-laws/src/main/kotlin/org/vorpal/kosmos/algebra/laws/AbelianGroupLaws.kt
+++ kosmos-laws/src/main/kotlin/org/vorpal/kosmos/algebra/laws/AbelianGroupLaws.kt
@@ -1,20 +1,21 @@
 package org.vorpal.kosmos.algebra.laws
 
 import io.kotest.property.Arb
+import org.vorpal.kosmos.algebra.structures.AbelianGroup
 import org.vorpal.kosmos.core.Eq
-import org.vorpal.kosmos.laws.property.CommutativityLaws
+
 
 /**
  * An Abelian group follows group laws and must be commutative.
  */
-open class AbelianGroupLaws<A, S>(
-    S: S,
+open class AbelianGroupLaws<A: Any, S>(
+    candidate: S,
     arb: Arb<A>,
-    EQ: Eq<A>
-) : GroupLaws<A, S>(S, arb, EQ)
-        where S : AbelianGroup<A, *> {
+    eq: Eq<A>
+) : GroupLaws<A, S>(candidate, arb, eq)
+        where S : AbelianGroup<A> {
 
-    private val commutativity = CommutativityLaws(S, arb, EQ)
+    private val commutativity = CommutativityLaws(candidate, arb, eq)
 
     override suspend fun all() {
         super.all()
---
+++ kosmos-testkit/README.md
@@ -0,0 +1,5 @@
+# kosmos-testkit
+
+Responsibility: “Utilities to help write tests against kosmos-laws.”
+
+This is where arbitraries should live.
