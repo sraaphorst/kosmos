332f35cf89f916ab30317b8681f78978cb0f74d8
Sebastian Raaphorst <sraaphorst@gmail.com>
2026-01-04 17:16:28 -0500

    K63: WIP.

--- kosmos-rec/src/main/kotlin/org/vorpal/kosmos/rec/cjk/CjkSettings.kt
+++ kosmos-rec/src/main/kotlin/org/vorpal/kosmos/rec/cjk/CjkSettings.kt
@@ -1,83 +1,97 @@
 package org.vorpal.kosmos.rec.cjk
 
-import org.vorpal.kosmos.functional.datastructures.Option
 import org.vorpal.kosmos.std.Rational
 import java.math.BigDecimal
 import java.math.BigInteger
 
 
-enum class Script(val englishName: String, val nativeName: String) {
-    ZH_HANT("Chinese (Traditional)", "中文繁體"),
-    ZH_HANS("Chinese (Simplified)", "中文简体"),
-    JP("Japanese", "日本語");
-
-
+sealed class Locale(
+    val englishName: String,
+    val nativeName: String
+) {
     override fun toString(): String =
         "$englishName / $nativeName"
 }
 
-private val ZHScripts: Set<Script> = setOf(Script.ZH_HANT, Script.ZH_HANS)
-private val ZHTradScript: Set<Script> = setOf(Script.ZH_HANT)
-private val ZHSimpScript: Set<Script> = setOf(Script.ZH_HANS)
-private val JPScript: Set<Script> = setOf(Script.JP)
-private val AllScripts: Set<Script> = Script.entries.toSet()
+sealed class HanLocale(englishName: String, nativeName: String): Locale(englishName, nativeName) {
+    object ZH_HANT: HanLocale("Chinese (Traditional)", "中文繁體")
+    object ZH_HANS: HanLocale("Chinese (Simplified)", "中文简体")
+}
 
-sealed class ScriptTyped(
-    private val scripts: Set<Script>,
+sealed class JapaneseLocale(englishName: String, nativeName: String) : Locale(englishName, nativeName) {
+    object JP : JapaneseLocale("Japanese", "日本語")
+}
+
+private val ZHLocaleSet: Set<Locale> = setOf(HanLocale.ZH_HANT, HanLocale.ZH_HANS)
+private val ZHTradLocaleSet: Set<Locale> = setOf(HanLocale.ZH_HANT)
+private val ZHSimpLocaleSet: Set<Locale> = setOf(HanLocale.ZH_HANS)
+private val JPLocaleSet: Set<Locale> = setOf(JapaneseLocale.JP)
+private val FullLocaleSet: Set<Locale> = ZHLocaleSet + JPLocaleSet
+
+sealed class LocaleTyped(
+    private val locales: Set<Locale>,
     private val description: String)
 {
     init {
-        require(scripts.isNotEmpty()) { "No script associated with setting $description" }
+        require(locales.isNotEmpty()) { "No locale associated with setting $description" }
     }
 
-    private val scriptRepresentation: String by lazy {
-        when (scripts.size) {
-            1 -> scripts.single().toString()
-            else -> scripts.joinToString(separator = ", ")
+    private val localeRepresentation: String by lazy {
+        when (locales.size) {
+            1 -> locales.single().toString()
+            else -> locales.joinToString(separator = ", ")
         }
     }
 
-    fun checkCompatability(selectedScript: Script) {
-        require(selectedScript in scripts) {
-            "Script incompatibility: selected is $selectedScript, permitted: $scriptRepresentation"
+    fun checkCompatibility(selectedLocale: Locale) {
+        require(selectedLocale in locales) {
+            "Locale incompatibility: selected is $selectedLocale, permitted: $localeRepresentation"
         }
     }
 }
 
-sealed class ZeroStyle(val value: String, scripts: Set<Script>):
-    ScriptTyped(scripts, "ZeroStyle") {
+sealed class ZeroStyle(val value: String, locales: Set<Locale>):
+    LocaleTyped(locales, "ZeroStyle") {
 
-    object Chinese: ZeroStyle("零", ZHScripts)
-    object JapaneseRei: ZeroStyle("零", JPScript)
-    object JapaneseCircle: ZeroStyle("〇", JPScript)
+    object Chinese: ZeroStyle("零", ZHLocaleSet)
+    object JapaneseKanji: ZeroStyle("零", JPLocaleSet)
+    object JapaneseCircle: ZeroStyle("〇", JPLocaleSet)
+}
+private val zeros: Set<String> = setOf(
+    ZeroStyle.Chinese.value,
+    ZeroStyle.JapaneseKanji.value,
+    ZeroStyle.JapaneseCircle.value
+)
+
+sealed class PositiveStyle(val prefix: String, locales: Set<Locale>):
+    LocaleTyped(locales, "PositiveStyle") {
+
+    object Omit: PositiveStyle("", FullLocaleSet)
+    object Plus: PositiveStyle("+", FullLocaleSet)
+    object ChineseZheng: PositiveStyle("正", ZHLocaleSet)
+    object JapaneseKatakana: PositiveStyle("プラス", JPLocaleSet)
 }
 
-sealed class PositiveStyle(val prefix: String, scripts: Set<Script>):
-    ScriptTyped(scripts, "PositiveStyle") {
+sealed class NegativeStyle(val prefix: String, locales: Set<Locale>):
+    LocaleTyped(locales, "NegativeStyle") {
 
-    object Omit: PositiveStyle("", AllScripts)
-    object Plus: PositiveStyle("+", AllScripts)
-    object ChineseZheng: PositiveStyle("正", ZHScripts)
-    object JapaneseKatakana: PositiveStyle("プラス", JPScript)
+    object Minus: NegativeStyle("-", FullLocaleSet)
+    object TraditionalChineseFu: NegativeStyle("負", ZHTradLocaleSet)
+    object SimplifiedChineseFu: NegativeStyle("负", ZHSimpLocaleSet)
+    object JapaneseKatakana: NegativeStyle("マイナス", JPLocaleSet)
 }
 
-sealed class NegativeStyle(val prefix: String, scripts: Set<Script>):
-    ScriptTyped(scripts, "NegativeStyle") {
+sealed class DecimalPointStyle(val point: String, locales: Set<Locale>):
+    LocaleTyped(locales, "DecimalPointStyle") {
 
-    object Minus: NegativeStyle("-", AllScripts)
-    object TraditionalChineseFu: NegativeStyle("負", ZHTradScript)
-    object SimplifiedChineseFu: NegativeStyle("负", ZHSimpScript)
-    object JapaneseKatakana: NegativeStyle("マイナス", JPScript)
-}
-
-sealed class DecimalPointStyle(val point: String, scripts: Set<Script>):
-    ScriptTyped(scripts, "DecimalPointStyle") {
-
-    object Point: DecimalPointStyle(".", AllScripts)
-    object CJKPoint: DecimalPointStyle("．", AllScripts)
-    object Comma: DecimalPointStyle(",", AllScripts)
-    object TraditionalChinese: DecimalPointStyle("點", ZHTradScript)
-    object SimplifiedChinese: DecimalPointStyle("点", ZHSimpScript)
+    object CJKPoint: DecimalPointStyle("．", FullLocaleSet)
+    object CJKMiddleDot: DecimalPointStyle("・", JPLocaleSet)
+    object CJKPeriod: DecimalPointStyle("。", FullLocaleSet)
+    object CJKComma: DecimalPointStyle("，", FullLocaleSet)
+    object TraditionalChineseDian: DecimalPointStyle("點", ZHTradLocaleSet)
+    object SimplifiedChineseDian: DecimalPointStyle("点", ZHSimpLocaleSet)
+    object Point: DecimalPointStyle(".", FullLocaleSet)
+    object Comma: DecimalPointStyle(",", FullLocaleSet)
 }
 
 /**
@@ -108,39 +122,40 @@ enum class DigitStyle {
  * [Never] means:
  * - 二 will always be used, regardless of position.
  *
- * [TraditionalChineseSpoken] and [SimplifiedChineseSpoken] mean:
+ * [TraditionalChineseLiang] and [SimplifiedChineseLiang] mean:
  * - 二 will be used before 十 / 百.
  * - 兩 / 两 will be used before 百 / 千 / 萬 / 億 /...
  */
-sealed class LiangPolicy(val value: String, scripts: Set<Script>): ScriptTyped(scripts, "LiangPolicy") {
-    object Never: LiangPolicy("二", ZHScripts)
-    object TraditionalChineseSpoken: LiangPolicy("兩", ZHTradScript)
-    object SimplifiedChineseSpoken: LiangPolicy("两", ZHSimpScript)
+sealed class LiangPolicy(val value: String, locales: Set<Locale>): LocaleTyped(locales, "LiangPolicy") {
+    object Never: LiangPolicy("二", ZHLocaleSet)
+    object TraditionalChineseLiang: LiangPolicy("兩", ZHTradLocaleSet)
+    object SimplifiedChineseLiang: LiangPolicy("两", ZHSimpLocaleSet)
 }
 
 
-internal typealias ScriptDict = Map<Script, String>
+internal typealias LocaleDict = Map<Locale, String>
 
 /**
- * Create a map from Script to representation. Note that this is flexible to simplify the case where
- * the simplified Chinese character
+ * Create a map from [Locale] to representation. Note that this is flexible to simplify the case where
+ * the simplified Chinese character either differs or doesn't differ from the traditional character.
  */
-private fun makeMap(zhTrad: String, zhSimp: String? = null, jp: String? = null): ScriptDict =
+private fun makeMap(zhTrad: String, zhSimp: String? = null, jp: String? = null): LocaleDict =
     buildMap {
-        put(Script.ZH_HANT, zhTrad)
-        put(Script.ZH_HANS, (zhSimp ?: zhTrad))
-        if (jp != null) put(Script.JP, jp)
+        put(HanLocale.ZH_HANT, zhTrad)
+        put(HanLocale.ZH_HANS, (zhSimp ?: zhTrad))
+        if (jp != null) put(JapaneseLocale.JP, jp)
     }
 
 /**
- * Note: standard is identical across all three scripts.
+ * Note: standardChar is identical across all three locales, and is always used when financial mode
+ * is enabled and there is no entry for the financial mode character (which happens frequently in
+ * Japanese).
  */
 internal enum class SmallNumbers(
     val value: Int,
     val standardChar: String,
-    val financialChars: ScriptDict
+    val financialChars: LocaleDict
 ) {
-    LING(0, "零", makeMap("零", "零", "零")),
     YI(1, "一", makeMap("壹", "壹", "壱")),
     ER(2, "二", makeMap("貳", "贰", "弐")),
     SAN(3, "三", makeMap("參", "叁", "参")),
@@ -158,7 +173,8 @@ internal enum class SmallNumbers(
 
 internal enum class Myriad(
     val power10: Int,
-    val chars: ScriptDict
+    val chars: LocaleDict,
+    val lowerValue: BigInteger = BigInteger.TEN.pow(power10),
 ) {
     WAN(4, makeMap("萬", "万", "万")),
     YI(8, makeMap("億", "亿", "億")),
@@ -181,13 +197,14 @@ internal enum class Myriad(
 
 
 /**
- * Information required for taking an arabic number and formatting it in Chinese characters.
+ * Information required for taking an Arabic number and formatting it in Chinese characters.
  */
 sealed interface CjkNumberFormat {
-    val script: Script
+    val locale: Locale
     val positiveStyle: PositiveStyle
     val negativeStyle: NegativeStyle
     val digitStyle: DigitStyle
+    val zeroStyle: ZeroStyle
     val decimalPointStyle: DecimalPointStyle
     val fractionStyle: FractionStyle
 
@@ -196,33 +213,104 @@ sealed interface CjkNumberFormat {
     fun format(rational: Rational): String
     fun formatPercent(decimal: BigDecimal): String
 }
+private fun SmallNumbers.charFor(
+    locale: Locale,
+    digitStyle: DigitStyle
+): String =
+    if (digitStyle == DigitStyle.FINANCIAL) financialChars[locale] ?: standardChar
+    else standardChar
 
+private fun findSmallNumber(value: Int): SmallNumbers =
+    SmallNumbers.entries.first { it.value == value }
+
+private fun unitChar(
+    pos: Int,
+    locale: Locale,
+    digitStyle: DigitStyle
+): String {
+    val unit = findSmallNumber(pos)
+    return unit.charFor(locale, digitStyle)
+}
 
 class ChineseNumberFormat(
-    override val script: Script,
+    override val locale: HanLocale,
     override val positiveStyle: PositiveStyle = PositiveStyle.Omit,
-    override val negativeStyle: NegativeStyle = defaultNegativeStyle(script),
+    override val negativeStyle: NegativeStyle = defaultNegativeStyle(locale),
     override val digitStyle: DigitStyle = DigitStyle.COMMON,
-    val liangPolicy: LiangPolicy = defaultLiangPolicy(script),
-    override val decimalPointStyle: DecimalPointStyle = defaultDecimalStyle(script),
+    val liangPolicy: LiangPolicy = defaultLiangPolicy(locale),
+    override val decimalPointStyle: DecimalPointStyle = defaultDecimalStyle(locale),
     override val fractionStyle: FractionStyle = FractionStyle.MIXED
-): CjkNumberFormat, ScriptTyped(ZHScripts, "ChineseNumberFormat") {
+): CjkNumberFormat, LocaleTyped(ZHLocaleSet, "ChineseNumberFormat") {
+    override val zeroStyle: ZeroStyle = ZeroStyle.Chinese
+
     init {
-        checkCompatability(script)
-        positiveStyle.checkCompatability(script)
-        negativeStyle.checkCompatability(script)
-        liangPolicy.checkCompatability(script)
-        decimalPointStyle.checkCompatability(script)
+        checkCompatibility(locale)
+        positiveStyle.checkCompatibility(locale)
+        negativeStyle.checkCompatibility(locale)
+        liangPolicy.checkCompatibility(locale)
+        zeroStyle.checkCompatibility(locale)
+        decimalPointStyle.checkCompatibility(locale)
+    }
+
+    override fun format(bigInteger: BigInteger): String {
+        // Format a single digit at a certain position.
+        fun formatDigit(value: Int, position: Int): String =
+            if (value > 0) {
+                val smallNumber = findSmallNumber(value)
+
+                // Financial style always takes precedence.
+                if (digitStyle == DigitStyle.FINANCIAL)
+                    smallNumber.financialChars.getOrDefault(locale, smallNumber.standardChar)
+                else if (value == 2 && (position == 1000 || position == 100))
+                    liangPolicy.value
+                else
+                    smallNumber.standardChar
+                }
+            else ""
+
+        // Format a myriad group.
+        fun formatMyriad(group: Int): String =
+            listOf(1000, 100, 10, 1).fold(group to "") { (remain, str), pos ->
+                val nextRemain = remain % pos
+                val digitSymbol = formatDigit(remain / pos, pos)
+                if (digitSymbol.isNotEmpty()) {
+                    // We only want the position symbol for 10, 100, 1000.
+                    val posSymbol = if (pos > 1) {
+                        val posLookup = findSmallNumber(pos)
+                        if (digitStyle == DigitStyle.FINANCIAL) posLookup.financialChars.getOrDefault(
+                            locale,
+                            posLookup.standardChar
+                        )
+                        else posLookup.standardChar
+                    } else ""
+
+                    nextRemain to ("$str$digitSymbol$posSymbol")
+                } else nextRemain to str
+            }.second
+
+        // Iterate over the myriads in reverse order and determine if they apply.
+        val unsignedFormat = Myriad.entries.reversed().fold(bigInteger.abs() to "") { (remain, str), myriad ->
+            val nextRemain = remain % myriad.lowerValue
+            when (val group = remain / myriad.lowerValue) {
+                BigInteger.ZERO if str.isEmpty() -> nextRemain to str
+                BigInteger.ZERO if str.last().toString() !in zeros -> nextRemain to (str + zeroStyle.value + myriad.chars)
+                else -> nextRemain to (str + formatMyriad(group.toInt()) + myriad.chars)
+            }
+        }.second
+
+        return if (bigInteger > BigInteger.ZERO) positiveStyle.prefix + unsignedFormat
+        else if (bigInteger < BigInteger.ZERO) negativeStyle.prefix + unsignedFormat
+        else unsignedFormat
     }
 
     companion object {
-        private fun <A : Any> defaulter(zhHant: A, zhHans: A): (Script) -> A = {
-            if (it == Script.ZH_HANT) zhHant else zhHans
+        private fun <A : Any> defaulter(zhHant: A, zhHans: A): (HanLocale) -> A = {
+            if (it == HanLocale.ZH_HANT) zhHant else zhHans
         }
 
         internal val defaultLiangPolicy = defaulter(
-            LiangPolicy.TraditionalChineseSpoken,
-            LiangPolicy.SimplifiedChineseSpoken
+            LiangPolicy.TraditionalChineseLiang,
+            LiangPolicy.SimplifiedChineseLiang
         )
 
         internal val defaultNegativeStyle = defaulter(
@@ -231,8 +319,8 @@ class ChineseNumberFormat(
         )
 
         internal val defaultDecimalStyle = defaulter(
-            DecimalPointStyle.TraditionalChinese,
-            DecimalPointStyle.SimplifiedChinese
+            DecimalPointStyle.TraditionalChineseDian,
+            DecimalPointStyle.SimplifiedChineseDian
         )
     }
 }
@@ -241,15 +329,17 @@ class JapaneseNumberFormat(
     override val positiveStyle: PositiveStyle = PositiveStyle.Omit,
     override val negativeStyle: NegativeStyle = NegativeStyle.JapaneseKatakana,
     override val digitStyle: DigitStyle = DigitStyle.COMMON,
-    override val decimalPointStyle: DecimalPointStyle = DecimalPointStyle.Point,
+    override val zeroStyle: ZeroStyle = ZeroStyle.JapaneseCircle,
+    override val decimalPointStyle: DecimalPointStyle = DecimalPointStyle.CJKPoint,
     override val fractionStyle: FractionStyle = FractionStyle.MIXED,
-): CjkNumberFormat, ScriptTyped(JPScript, "JapaneseNumberFormat") {
-    override val script = Script.JP
+): CjkNumberFormat, LocaleTyped(JPLocaleSet, "JapaneseNumberFormat") {
+    override val locale: JapaneseLocale = JapaneseLocale.JP
 
     init {
-        checkCompatability(script)
-        positiveStyle.checkCompatability(script)
-        negativeStyle.checkCompatability(script)
-        decimalPointStyle.checkCompatability(script)
+        checkCompatibility(locale)
+        positiveStyle.checkCompatibility(locale)
+        negativeStyle.checkCompatibility(locale)
+        zeroStyle.checkCompatibility(locale)
+        decimalPointStyle.checkCompatibility(locale)
     }
 }
